0488aa180482a18d061a6dc7c0bcf1e71d76bc1d
igorntwari <igorntwari28@gmail.com>
2025-09-04 20:08:02 +0200

    feat: add proper fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="font-articulat-regular text-9xl">Hello world</h1>
+      <h1 className="font-articulat-semibold text-9xl">Hello world</h1>
+      <h1 className="font-articulat-Extrabold text-9xl">Hello world</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00F87C",
+        secondary: "#D9D9D9",
+        cream: "#F7F7F7",
+      },
+      fontFamily: {
+        "articulat-regular": ["Articulat-regular"],
+        "articulat-semibold": ["Articulat-semibold"],
+        "articulat-Extrabold": ["Articulat-Extrabold"],
+      },
+    },
+  },
+  plugins: [],
+};
