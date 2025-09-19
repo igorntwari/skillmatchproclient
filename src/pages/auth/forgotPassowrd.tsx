8758263395444b974b01fc3fd14234b464c3f76e
igorntwari <igorntwari28@gmail.com>
2025-09-19 14:38:17 +0200

    feat: add forget screen

--- src/pages/auth/forgotPassowrd.tsx
+++ src/pages/auth/forgotPassowrd.tsx
@@ -7,16 +7,16 @@ import BackIcon from "../../assets/icons/Back.svg";
 
 function ForgotPassword() {
   return (
-    <div className="flex justify-center align-center h-full border border-auth_border">
+    <div className="flex justify-center mx-auto align-center h-full border border-b-4 border-auth_border w-[410px] mt-10 rounded-3xl">
       <form
         // onSubmit={}
-        className="bg-no-repeat bg-cover p-16 py-16 px-5 md:p-16 md:px-32 space-y-10 flex flex-col items-start justify-center w-[410px]"
+        className="bg-no-repeat bg-cover p-16 py-16 px-5 md:p-16 md:px-32 space-y-10 flex flex-col items-start justify-center"
         style={{ backgroundImage: "url('/images/scratchesBack.webp')" }}
       >
         <ProgressiveBar step={2} />
         <div className="flex flex-col items-center gap-3">
           <TypographyH2>Forgot your password?</TypographyH2>
-          <span className="text-base">
+          <span className="text-base text-center">
             A code will be sent to your email to help reset <br /> password
           </span>
         </div>
